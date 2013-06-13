4ecf195120d1ab746d4a635cea1775e2b3889406
softprops <d.tangren@gmail.com>
2013-06-13 08:16:38 -0400

    progres listener

--- src/main/scala/package.scala
+++ src/main/scala/package.scala
@@ -1,9 +1,34 @@
 package object bintry {
-  import com.ning.http.client.Response
+  import com.ning.http.client.{ AsyncHandler, ProgressAsyncHandler, Response }
   import dispatch.FunctionHandler
 
-
   object Noop extends FunctionHandler(identity)
+
+  /*object as {
+    object Progress {
+      import AsyncHandler.STATE
+      import STATE._
+      def apply(onProgress: (Long, Long, Long) => Unit, onHeaderComplete: () => Unit = {()=>()}, onComplete: () => Unit = {()=>()}) =
+        new ProgressAsyncHandler[Unit] {
+          val state = CONTINUE
+
+          def onHeaderWriteCompleted(): STATE = {
+            onHeaderComplete()
+            state
+          }
+
+          def onContentWriteCompleted(): STATE = {
+            onComplete()
+            state
+          }
+
+          def onContentWriteProgress(amount: Long, current: Long, total: Long): Unit = {
+            onProgress(amount, current, total)
+            state
+          }
+        }
+    }
+  }*/
 
   // use this when dropping 2.9.3 support
   //implicit class ImplicitFunctionHandler[T](f: Response => T) extends FunctionHandler(f)
