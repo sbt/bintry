66ca79ae6e101615ed1a8567e5cde0f4b79ac303
softprops <d.tangren@gmail.com>
2013-06-12 21:56:56 -0400

    a noop handler

--- src/main/scala/package.scala
+++ src/main/scala/package.scala
@@ -2,6 +2,9 @@ package object bintry {
   import com.ning.http.client.Response
   import dispatch.FunctionHandler
 
+
+  object Noop extends FunctionHandler(identity)
+
   // use this when dropping 2.9.3 support
   //implicit class ImplicitFunctionHandler[T](f: Response => T) extends FunctionHandler(f)
 
