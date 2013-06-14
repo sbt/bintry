79e487423b36189a625027152ad2b4f017b16d0d
softprops <d.tangren@gmail.com>
2013-06-14 01:06:15 -0400

    progress

--- src/main/scala/package.scala
+++ src/main/scala/package.scala
@@ -1,34 +1,63 @@
 package object bintry {
-  import com.ning.http.client.{ AsyncHandler, ProgressAsyncHandler, Response }
+  import com.ning.http.client.{ AsyncCompletionHandler, AsyncHandler, ProgressAsyncHandler, Response }
   import dispatch.FunctionHandler
 
   object Noop extends FunctionHandler(identity)
 
-  /*object as {
+  object having {
+    import AsyncHandler.STATE
+    import STATE._
+
+    case class Step(amount: Long, current: Long, total: Long)
+
+    /** companion builder for the common case of Progress */
     object Progress {
-      import AsyncHandler.STATE
-      import STATE._
-      def apply(onProgress: (Long, Long, Long) => Unit, onHeaderComplete: () => Unit = {()=>()}, onComplete: () => Unit = {()=>()}) =
-        new ProgressAsyncHandler[Unit] {
-          val state = CONTINUE
-
-          def onHeaderWriteCompleted(): STATE = {
-            onHeaderComplete()
-            state
-          }
-
-          def onContentWriteCompleted(): STATE = {
-            onComplete()
-            state
-          }
-
-          def onContentWriteProgress(amount: Long, current: Long, total: Long): Unit = {
-            onProgress(amount, current, total)
-            state
-          }
-        }
+      def apply(onProgress: Step => Unit): Progress[Unit] =
+        Progress(onProgress, () => (), () => (), _ => ())
     }
-  }*/
+
+    /** a completion handler an progres listener all in one */
+    case class Progress[T](
+      _onProgress: Step => Unit,
+      _onHeaderComplete: () => Unit,
+      _onComplete: () => Unit,
+      _response: Response => T)
+      extends AsyncCompletionHandler[T] with ProgressAsyncHandler[T] {
+
+      @volatile private var state = CONTINUE
+
+      def onComplete(complete: () => Unit) =
+        Progress(_onProgress, _onHeaderComplete, complete, _response)
+
+      def onHeaderComplete(complete: () => Unit) =
+        Progress(_onProgress, complete, _onComplete, _response)
+
+      def response[T](response: Response => T) =
+        Progress(_onProgress, _onHeaderComplete, _onComplete, response)
+
+      final def onCompleted(response: Response) =
+        _response(response)
+
+      final override def onHeaderWriteCompleted(): STATE = {
+        _onHeaderComplete()
+        state
+      }
+
+      final override def onContentWriteCompleted(): STATE = {
+        _onComplete()
+        state
+      }
+
+      final override def onContentWriteProgress(amount: Long, current: Long, total: Long): STATE = {
+        _onProgress(Step(amount, current, total))
+        state
+      }
+
+      final def stop() {
+        state = ABORT
+      }
+    }
+  }
 
   // use this when dropping 2.9.3 support
   //implicit class ImplicitFunctionHandler[T](f: Response => T) extends FunctionHandler(f)
